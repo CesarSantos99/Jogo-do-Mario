fa0c33e58d39f2f6126e7b221681b75da2584071
César <cesarsantossouza99@gmail.com>
2023-05-15 20:23:39 -0300

    fim

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 const mario = document.querySelector('.mario')
+const pipe = document.querySelector('.pipe')
+
 
 const jump = () => {
     mario.classList.add('jump') // add uma classe na classe mario
@@ -8,7 +10,26 @@ const jump = () => {
     }, 500)
 } // função que add uma classe na outra, ai pula e depois remove o pulo
 
+const loop = setInterval (() => {
+    const pipePosition = pipe.offsetLeft
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
 
+
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+        
+        pipe.style.animation = 'none'
+        pipe.style.left = `${pipePosition}px`
+
+        mario.style.animation = 'none'
+        mario.style.bottom = `${marioPosition}px`
+
+        mario.src = './img/game-over.png'
+        mario.style.width = '75px'
+        mario.style.marginLeft = '43px'
+
+        clearInterval(loop)
+    }
+},10)
 
 
 
